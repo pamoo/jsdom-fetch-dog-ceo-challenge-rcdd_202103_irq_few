e52955ee5479cd00991beaffef82dd9c68c5e3f8
Payam Abdullah <payam97abubakr@gmail.com>
2021-06-16 19:54:19 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,1 +1,41 @@
-console.log('%c HI', 'color: firebrick')
+let breeds = [];
+
+document.addEventListener('DOMContentLoaded', function () {
+  loadImages();
+  loadBreedOptions();
+  fetchDogImage();
+  fetchBreeds();
+});
+
+function loadImages() {
+function fetchDogImage() {
+  const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+  fetch(imgUrl)
+    .then(res=> res.json())
+@@ -21,7 +21,7 @@ function addImage(dogPicUrl) {
+  container.appendChild(newImageEl);
+}
+
+function loadBreedOptions() {
+function fetchBreeds() {
+  const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+  fetch(breedUrl)
+    .then(res => res.json())
+@@ -59,14 +59,14 @@ function addBreedSelectListener() {
+}
+
+function addBreed(breed) {
+  let ul = document.querySelector('#dog-breeds');
+  let li = document.createElement('li');
+  const ul = document.querySelector('#dog-breeds');
+  const li = document.createElement('li');
+  li.innerText = breed;
+  li.style.cursor = 'pointer';
+  ul.appendChild(li);
+  li.addEventListener('click', updateColor);
+}
+
+function updateColor(event) {
+  event.target.style.color = 'palevioletred';
+  event.target.style.color = 'skyblue';
+} 
